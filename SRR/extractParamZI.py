3a22b3261062afa1dedc2651dfe79f97abc1bbc4
bigot <bigot@bigot-PORTEGE-Z930.(none)>
2013-09-20 03:55:59 +0200

    mise a jour overlap dans zi

--- SRR/extractParamZI.py
+++ SRR/extractParamZI.py
@@ -1,7 +1,7 @@
 #! /usr/local/bin/python3.2
 # -*- coding: utf-8 -*-
 
-import sys,  os, re, time, numpy
+import sys,  os, re, time, numpy, copy
 from collections import defaultdict
 
 '''
@@ -57,10 +57,10 @@ def _getLengthParam(param, desc, submatrix):
 	#~ l=lambda:defaultdict(l)
 	#~ spk=l()
 	param['speaker'] = dict()
-		
-	for speaker in desc[0] : 
+	for speaker in desc[0][0] : 
 		param['speaker'][speaker] = dict()
 		temp = []
+		
 		for line in submatrix:
 			if line[0] == speaker : 
 				temp.append((line[2] - line[1])/100.0)
@@ -85,15 +85,14 @@ def _getLengthParam(param, desc, submatrix):
 		param['speaker']['silence']['stdLength']     = 0
 		param['speaker']['silence']['duration']  = 0
 		param['speaker']['silence']['counterSeg'] = 0
-			
 	return param 
 #---------------------------------------------------#
 def _getSpeakerContribution(desc, param):
 	cumulateSpeech = 0;
-	for speaker in desc[0]:
+	for speaker in desc[0][0]:
 		cumulateSpeech +=  float(param['speaker'][speaker]['duration'])
 
-	for speaker in desc[0] :
+	for speaker in desc[0][0] :
 		param['speaker'][speaker]['contrib'] = "%0.2f"%(100.0 * float(param['speaker'][speaker]['duration'])/(cumulateSpeech)) 
 	return param
 #---------------------------------------------------# 
@@ -131,18 +130,49 @@ def _getSpeakerContribution(desc, param):
 def extractParam(matrix, interaction):
 	''' c'est la zone qui est caracterisée pas le locuteur '''
 	output = []
+	keptZone = []
 	for zone in interaction :
-		param = dict()
-		param['iLevel']  = _getInteractionLevel(zone, matrix[zone[1]: zone[2]+1])
-		param['idStart'] = zone[1]
-		param['idEnd']   = zone[2]
+		iLevel  = _getInteractionLevel(zone, matrix[zone[1]: zone[2]+1])
+		if iLevel > 0 :
+			x = copy.deepcopy(matrix[zone[1]: zone[2]+1])
+			keptZone.append( [zone ,  iLevel , x ])
 			
-		if param['iLevel'] > 0 :
-			param['iDuration']  = _getInteractionDuration( zone, matrix[zone[1]: zone[2]+1] )
-			param['iDynamic']   = _getInteractionDynamic(param['iLevel'] , param['iDuration'] )
-			param			 	= _getLengthParam(param,  zone, matrix[zone[1]: zone[2]+1] )
-			param				= _getSpeakerContribution(zone, param)     
-			output.append(param)
+	#~ print keptZone
+	
+	coverSeg = []
+	for i in range(len(keptZone)-1) : 
+		#~ print keptZone[i]
+		if keptZone[i][0][2] - keptZone[i+1][0][1] == 0:
+			# modification directe
+			a = keptZone[i][2][len(keptZone[i][2])-1][1]
+			b = keptZone[i][2][len(keptZone[i][2])-1][2]
+			c = int(a + (b-a)/2)
+			#~ print a, b, c
+			#~ keptZone[i][2][len(keptZone[i][2])-1][1] = a
+			keptZone[i][2][len(keptZone[i][2])-1][2] = c
+			keptZone[i+1][2][0][1] = c + 1
+			
+			#~ print keptZone[i][2][len(keptZone[i][2])-1][2]
+			#~ print a, b, c
+			#~ print keptZone[i], '\n'
+	#~ exit()		
+			
+		#~ print param['iLevel'], matrix[zone[1]: zone[2]+1]
+	for zone in keptZone :
+		param = dict()
+		param['iLevel'] = zone[1]
+		#~ print param['iLevel']
+			
+		#~ param['idStart'] = zone[1]
+		#~ param['idEnd']   = zone[2]
+		#~ if param['iLevel'] > 0 :
+		param['iDuration']  = _getInteractionDuration( zone, zone[2] )
+		param['iDynamic']   = _getInteractionDynamic(param['iLevel'] , param['iDuration'] )
+		param			 	= _getLengthParam(param,  zone, zone[2] )
+		param				= _getSpeakerContribution(zone, param)     
+		param['speechTurns'] = zone[2]
+		output.append(param)
+	#~ print output
 	return output
 	
 	
--- SRR/extractingInteraction.py
+++ SRR/extractingInteraction.py
@@ -11,12 +11,16 @@ output are zi.param and zi.desc files
 '''         
 #------------------------------------------------------------------------------------------#
 def _validateZone(zone, speakerInvolved):
-	''' check the number of speaker involved in the zone'''
+	''' check the number of speaker involved in the zone
+		and the number of turns in the sequence '''
 	currentSpeaker = zone[0]
 	if len(currentSpeaker) != speakerInvolved:
 		return False
-	else: 
-		return True
+	else:
+		if zone[2] - zone[1] > 2 :
+			return True
+		else :
+			return False
 #------------------------------------------------------------------------------------------#
 def _mutualZone(matrix, index, nbSpk, gap):
 	''' parie concernant la gestion des segments de non parole : a verifier '''
@@ -32,6 +36,8 @@ def _mutualZone(matrix, index, nbSpk, gap):
 			pass
 		elif (matrix[j][0] == 'NonSpeech')   and  (matrix[j][2] - matrix[j][1]  > gap ) :
 			return [sorted(speakerInvolved), index , j - 1] 
+			
+			
 		if ( j == len(matrix) -1 ):
 				return [sorted(speakerInvolved), index , j]	
 #----------------------------------------------------------------------------------------------------#
@@ -52,32 +58,57 @@ def _lookingForInteraction(matrixIn, interval, speakerInvolved) :
 		# == recherche de la plus longue zone d'interaction entre N locuteurs à partir d'une position i == #
 		# zone = [[spk1,spk2],idDebut , idFin ]
 		zone 		= _mutualZone(matrixIn, i , speakerInvolved, interval)
-		
+				
+		# premier test sur le nombre de locuteurs impliqués et la longueur de la zone
 		if _validateZone(zone, speakerInvolved):
 			interestZone.append(zone)
+	#second test = retrait de la redondance.
 	keptZone  = _removeRedundant(interestZone)
+
+	
+	# third test : removing nonSpeech segments beginning or ending a zone
+	for h in keptZone : 
+		if matrixIn[ h[2]][0] == 'NonSpeech' :
+			h[2] = h[2] - 1
+
+		if matrixIn[ h[1]][0] == 'NonSpeech' :
+			h[1] = h[1] + 1 
+	
 	return keptZone
 #---------------------------------------------------------------------------------------------------#
-def writeXmlFile(param, matrix, outFile, nbSpk):
+def writeXmlFile(param, outFile, nbSpk):
+	
+	
 	fileContent = []
 	fileContent.append('<?xml version="1.0" encoding="UTF-8"?>')
 	fileContent.append('<interactionZones author="bbigot" version="0.3" date="' + str(datetime.date.today()) + '"  nbSpk="' + str(nbSpk) + '">')
 	for show in param:	
+		#~ for zone in param[show]: 
+			#~ print zone
+		#~ exit()
+		
+		
 		for zone in param[show]: 
+			
+			#~ print zone
 			# == global zone informations == #
 			# ------------------------------ #
 			line = '<zi show="'+show+'" ' 
 			for item in zone:
-				if  item != 'speaker' and item != 'silence':
+				if  item != 'speaker' and item != 'silence' and item != 'speechTurns':
 					line +=  item +'="'+ str(zone[item]) + '" '
-			line += 'starTime="' + str( matrix[show][int(zone['idStart'])][1] /100.0 ) +'" endTime="' + str( matrix[show][int(zone['idEnd'])][2] /100.0 ) +'"'
+
+
+			line += 'starTime="' + str( zone['speechTurns'][0][1]/100.0 ) +'" endTime="' + str( zone['speechTurns'][-1][2]/100.0 ) +'"'
 			line += '>'
 			fileContent.append(line)
 			
 			# == speaker informations == #
 			# -------------------------- #
 			fileContent.append('<speakers>')
+
 			for item in zone['speaker']:
+
 				line ='<speaker name="' + item +'"'
 				for val in zone['speaker'][item]:
 					line +=  ' '+ val +'="'+ str(zone['speaker'][item][val]) + '"'
@@ -85,15 +116,21 @@ def writeXmlFile(param, matrix, outFile, nbSpk):
 				fileContent.append(line)
 			fileContent.append('</speakers>')
 
+			
+			
 			# == speech turns informations == #
 			# ------------------------------- #
 			fileContent.append('<turns>')
-			for turn in matrix[show][ zone['idStart']:  zone['idEnd']+1 ] :
+			for turn in zone['speechTurns'] :
 				line = '<turn ' + 'start="' + str(turn[1]/100.) +'" end="' + str(turn[2]/100.)  + '" speaker="' + turn[0] + '"/>'
 				fileContent.append(line)
 			fileContent.append('</turns>')
 			fileContent.append('</zi>')
+			
 	fileContent.append('</interactionZones>')
+	#~ for i in fileContent:
+		#~ print i
+	#~ exit()
 		
 	# == writing to outputFile == #
 	# --------------------------- #
@@ -122,9 +159,36 @@ def _loadCentiMatrix(centiMatFile):
 		exit()		
 	return centiMat	
 #----------------------------------------------------------------------------------#
+def _checkOverlappingInteractionZones(currentInteractionZones, matrixIn):
+	
+	listeIndexToChange = []
+	for i in range(0, len(currentInteractionZones)-1):
+		if currentInteractionZones[i][2] >= currentInteractionZones[i+1][1] : 
+			if (currentInteractionZones[i+1][1] - currentInteractionZones[i][2]) == 0 :
+				print 'oveflap de 1'
+				listeIndexToChange.append(currentInteractionZones[i+1][1])
+			elif (currentInteractionZones[i+1][1] - currentInteractionZones[i][2]) > 0 :
+				print 'large overlap not treated so far'
+				exit()
+	print listeIndexToChange
+	
+	
+	
+	for i in currentInteractionZones:
+		subsequence = []
+		print i 
+		for j in range(i[1], i[2]+1):
+			subsequence.append(matrixIn[j])
+		print subsequence
+	
+	#~ currentInteractionZones = dict()
+	#print interactionZones
+	#~ return currentInteractionZones
+	return 1
+#----------------------------------------------------------------------------------#
 def extractInteract(centiMat, nbSpk):
-	interactionZones = dict()
-	interactParam    = dict()
+	interactionZones    = dict()
+	interactParam       = dict()
 	for show in centiMat:
 		interactionZones[show] = _lookingForInteraction(centiMat[show], 100, nbSpk) 	
 		interactParam[show] = extractParamZI.extractParam(centiMat[show], interactionZones[show])
@@ -140,7 +204,13 @@ if __name__=='__main__':
 
 	# == centi mat is a dict indexed by show == #
 	centiMatrix 		= _loadCentiMatrix(args.inputFile)
+	#print centiMatrix
+	#exit()
 	# == interactionParam is a dict indexed by show and containing lists == #
 	interactionParam 	= extractInteract(centiMatrix,int(args.nbSpk))
-	writeXmlFile(interactionParam, centiMatrix, args.outputFile, int(args.nbSpk))
+	#~ for i in interactionParam:
+		#~ for j in interactionParam[i]:
+			#~ print j
+	#~ exit()
+	writeXmlFile(interactionParam, args.outputFile, int(args.nbSpk))
 
